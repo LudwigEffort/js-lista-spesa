894e2871861ae44ac0de26448da172cdf383bb66
LudwigVonEffort <dr.feynman65@gmail.com>
2022-10-27 19:19:51 +0200

    work done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,36 @@ Consigli:
 Come posso ciclare su un array con il ciclo while?
 */
 
+const arrGroceryList = [];
+
+const eleGroceryForm = document.querySelector('form'); // setto la const all'oggetto form (in questo caso query funziona senza id o classe?)
+const inputGroceryItem = document.querySelector('#input-grocery'); //setto input form
+const eleGroceryList = document.querySelector('.grocery-list'); //setto l'oggetto ol 
+
+// creiamo l'event listener (ovvero diamo funzione al form)
+eleGroceryForm.addEventListener('submit', 
+
+    function(event) {
+        event.preventDefault(); //così disattiviamo...
+
+        if (inputGroceryItem.value) {
+            
+            //aggiorno i dati
+            arrGroceryList.push(inputGroceryItem.value);
+
+            //aggiornare l'interfaccia
+            eleGroceryList.innerHTML += `<li>${inputGroceryItem.value}</li>`;
+
+            //svuotare l'input
+            inputGroceryItem.value = '';
+        }
+
+    }
+
+);
+
+let i = 1;
+while (i <= arrGroceryList.length) {
+    eleGroceryList.innerHTML += `<li>${arrGroceryList[i]}</li>`;
+    i++;
+}
